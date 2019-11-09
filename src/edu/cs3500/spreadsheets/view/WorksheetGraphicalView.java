2edf7137486715731ed73f6a02c48e10b6ab9b03
furcij <furci.j@husky.neu.edu>
2019-11-09 15:18:49 -0500

    made poopy comment

--- src/edu/cs3500/spreadsheets/view/WorksheetGraphicalView.java
+++ src/edu/cs3500/spreadsheets/view/WorksheetGraphicalView.java
@@ -5,35 +5,26 @@ import java.io.IOException;
 
 import javax.swing.*;
 
-import edu.cs3500.spreadsheets.model.WorksheetModel;
-import edu.cs3500.spreadsheets.model.cell.Cell;
-
 /**
  * Represents the graphical view of a Worksheet.
  */
 public class WorksheetGraphicalView extends JFrame implements WorksheetView {
 
-  private static int LENGTH = 1000;
-  private static int HEIGHT = 700;
-  private WorksheetModel<Cell> model;
-  private JPanel panel;
+  private static int LENGTH = 30;
+  private static int HEIGHT = 20;
+  private JTable j;
 
   /**
    * Constructor for the WorksheetGraphicalView.
-   * It makes a default, empty table.
    */
-  public WorksheetGraphicalView(WorksheetModel<Cell> model) {
-    JTable table = new JTable(25, 25);
+  public WorksheetGraphicalView() {
+    this.j = new JTable();
     //TODO: what do we do with the new JTable?
-
-    JScrollPane scroll = new JScrollPane(table);
+    JScrollPane scroll = new JScrollPane(this.j);
     //TODO: are we sure we want to use a JScrollPane?
-
     this.add(scroll);
-    this.setSize(LENGTH, HEIGHT);
+    this.setSize(2000, 2000);
     this.setVisible(true);
-
-    this.model = model;
   }
 
   @Override
@@ -45,9 +36,4 @@ public class WorksheetGraphicalView extends JFrame implements WorksheetView {
   public void paintComponents(Graphics g) {
     super.paintComponents(g);
   }
-
-//  public static void main(String[] args) {
-//    WorksheetGraphicalView view = new WorksheetGraphicalView();
-//    view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//  }
 }
