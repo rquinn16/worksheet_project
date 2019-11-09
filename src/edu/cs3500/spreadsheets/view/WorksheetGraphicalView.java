25dba8595188fc1a168b5f4728b6cd2ed27ef888
furcij <furci.j@husky.neu.edu>
2019-11-09 16:49:00 -0500

    made poopy comment

--- src/edu/cs3500/spreadsheets/view/WorksheetGraphicalView.java
+++ src/edu/cs3500/spreadsheets/view/WorksheetGraphicalView.java
@@ -1,12 +1,16 @@
 package edu.cs3500.spreadsheets.view;
 
-import java.awt.*;
-import java.io.IOException;
-
-import javax.swing.*;
-
+import edu.cs3500.spreadsheets.model.Worksheet;
 import edu.cs3500.spreadsheets.model.WorksheetModel;
+import edu.cs3500.spreadsheets.model.WorksheetReader;
 import edu.cs3500.spreadsheets.model.cell.Cell;
+import java.awt.Graphics;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 /**
  * Represents the graphical view of a Worksheet.
@@ -23,24 +27,34 @@ public class WorksheetGraphicalView extends JFrame implements WorksheetView {
    * It makes a default, empty table.
    */
   public WorksheetGraphicalView(WorksheetModel<Cell> model) {
-    JScrollPane scroll = new JScrollPane(table);
-    // TODO: are we sure we want to use a JScrollPane?
-
-    this.add(scroll);
-    this.setSize(LENGTH, HEIGHT);
-    this.setVisible(true);
     this.model = model;
-    this.table = new JTable(new BasicTableModel(this.model));
-    // TODO: what do we do with the new JTable?
   }
 
   @Override
   public void render() throws IOException {
-
+    JScrollPane scroll = new JScrollPane(table);
+    this.add(scroll);
+    this.setSize(LENGTH, HEIGHT);
+    this.table = new JTable(new BasicTableModel(this.model));
+    this.setVisible(true);
   }
 
   @Override
   public void paintComponents(Graphics g) {
     super.paintComponents(g);
+  }
+
+  public static void main(String[] args) {
+    try {
+      FileReader f = new FileReader(
+          new File("C:\\Users\\jfri9\\OneDrive\\Desktop\\WorksheetExample.txt"));
+      Worksheet builder = new Worksheet();
+      WorksheetReader.read(builder, f);
+      WorksheetModel<Cell> model = builder.createWorksheet();
+      new WorksheetGraphicalView(model).render();
+    }
+    catch(IOException e) {
+      System.out.println("Something went wrong.");
+    }
   }
 }
