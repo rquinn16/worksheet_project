9172e63b4a60b23c3fe445c2deb7324c963b1088
furcij <furci.j@husky.neu.edu>
2019-11-12 13:51:44 -0500

    made poopy comment

---
+++ src/edu/cs3500/spreadsheets/view/BasicTableColumnModel.java
@@ -0,0 +1,33 @@
+package edu.cs3500.spreadsheets.view;
+
+import edu.cs3500.spreadsheets.model.Coord;
+import javax.swing.table.DefaultTableColumnModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableModel;
+
+/**
+ * Class representing a model for the columns in our spreadsheet.
+ */
+public class BasicTableColumnModel extends DefaultTableColumnModel {
+
+  private TableModel t;
+  private int col;
+
+  /**
+   * Constructor for BasicTableColumnModel.
+   * @param t
+   */
+  public BasicTableColumnModel(TableModel t) {
+    this.t = t;
+    this.col = t.getColumnCount();
+    this.addAllColumns();
+  }
+
+  private void addAllColumns() {
+    for (int i = 0; i < this.t.getColumnCount(); i++) {
+      TableColumn toAdd = new TableColumn(i);
+      toAdd.setHeaderValue(Coord.colIndexToName(i + 1));
+      this.addColumn(toAdd);
+    }
+  }
+}
